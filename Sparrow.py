d44bd7b19c31710af95aabaf5f881d34cfcb8e42
futurevoid <tamersaeed710@gmail.com>
2022-02-07 01:35:43 +0300

    added many featrues

--- Sparrow.py
+++ Sparrow.py
@@ -1,20 +1,25 @@
 # create a discord bot that will automatically send messages after a certain amount of time has passed
 # (in this case, the time is set to 5 seconds)
 import asyncio
-from email import message
-import time
-import random
-import os
-import sys
-import json
-from matplotlib import image
-import requests
-import discord
-import aiohttp
 import datetime as dt
-from discord.ext import commands
+import json
+import os
+import random
+import sys
+import time
+from email import message
 
-# load the discord client
+import aiohttp
+import discord
+#from more_itertools import sliced
+import requests
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.utils import get
+from matplotlib import image
+from youtube_dl import YoutubeDL as ytdl
+
 client = discord.Client()
 
 
@@ -57,7 +62,9 @@ async def on_message(message):
         msg = f'Pong! 🏓 {message.author.mention} {int(ping)}ms'.format(message)
         await message.channel.send(msg)
     elif message.content.startswith('^qr') or message.content.startswith('/qr'):
-        msg = f'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={message.content[8:]}'.format(message)
+        sliced =message.content[4:]
+        slicedr = sliced.replace(" ", "%20") 
+        msg = f'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={slicedr}'.format(message)
         await message.channel.send(msg)
     # send a message to the channel every 24 hours
     while message.content.startswith("^autosend") or message.content.startswith("/autosend"):
@@ -70,10 +77,10 @@ async def on_message(message):
         await message.channel.send("+share")
     async with aiohttp.ClientSession() as cs:
         if message.content.startswith('^mems') or message.content.startswith('/mems'):
-            async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            async with cs.get('https://www.reddit.com/r/memes/top/.json?sort=top&t=day') as r:
                 res = await r.json()
-                embed = discord.Embed(title="Memes", description="Here are some memes", color=0x00ff00)
-                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                embed = discord.Embed(title="Meme GIF", description="Here is a meme gif", color=0x00ff00)
+                embed.set_image(url=res['data']['children'][random.randint(0,len(res['data']['children']))]['data']['url'])
                 await message.channel.send(embed=embed)
     if message.content.startswith('^cat') or message.content.startswith('/cat'):
         async with aiohttp.ClientSession() as cs:
@@ -100,23 +107,23 @@ async def on_message(message):
         embed = discord.Embed(title="Commands List", description="""
         Hello {0.author.mention} 
         ^help * displays this message 
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^lol * gives random jokes 
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^hello * says hello
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^log * shows some nerdy stuff
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^ping * pings the bot
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^qr * makes a qr code
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^mems * displays a random meme
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^cat * displays a random cat
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^catfact * displays a random cat fact
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        -----------------------------
         ^catgif * displays a random cat gif
          """.format(message), color=0x00ff00)
         #await message.channel.send('{0.author.mention}').format(message)
@@ -127,7 +134,7 @@ async def on_message(message):
             if user.guild_permissions.administrator:
                 await message.channel.send('{0.author.mention}'.format(message)+f"{message.mentions[0]} is an admin and cannot be muted")
             else:
-                await message.channel.send('{0.author.mention}'.format(message)+ "nah")
+                await message.channel.send('{0.author.mention}'.format(message)+ "user is muted")
                 await user.edit(mute=True)
         else:
             await message.channel.send('{0.author.mention}'.format(message)) 
@@ -147,10 +154,76 @@ async def on_message(message):
             if user.guild_permissions.administrator:
                 await message.channel.send('{0.author.mention}'.format(message)+f"{message.mentions[0]} is an admin and cannot be kicked")
             else:
-                await message.channel.send('{0.author.mention}'.format(message)+ "nah")
-                await user.kick()
+                await message.channel.send('{0.author.mention}'.format(message)+ "user is kicked")
+                await user.guild.kick(user)
         else:
-            await message.channel.send('{0.author.mention}'.format(message))                    
+            await message.channel.send('{0.author.mention}'.format(message))
+    elif message.content.startswith('^ban') or message.content.startswith('/ban'):
+        if message.author.guild_permissions.administrator:
+            user = message.mentions[0]
+            await message.channel.send('{0.author.mention}'.format(message)+ "user is banned")
+            await user.guild.ban(user)
+        else:
+            await message.channel.send('{0.author.mention}'.format(message))
+    elif message.content.startswith('^unban') or message.content.startswith('/unban'):
+        if message.author.guild_permissions.administrator:
+            user = message.mentions[0]
+            if user.guild_permissions.administrator:
+                await message.channel.send('{0.author.mention}'.format(message)+f"{message.mentions[0]} cannot be unbanned")
+            else:
+                await message.channel.send('{0.author.mention}'.format(message)+ "user is unbanned")
+                await user.guild.unban(user)
+        else:
+            await message.channel.send('{0.author.mention} not an admin'.format(message))
+    elif message.content.startswith('^clear') or message.content.startswith('/clear'):
+        if message.author.guild_permissions.administrator:
+            mc=int(message.content[7:])
+            await message.channel.purge(limit=mc)
+        else:
+            await message.channel.send('{0.author.mention} not an admin'.format(message))
+    elif message.content.startswith('^gmems') or message.content.startswith('/gmems'):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/gamingmemes/.json?limit=100') as r:
+                res = await r.json()
+                embed = discord.Embed(title="Gaming Memes", description="Here is a gaming meme", color=0x00ff00)
+                embed.set_image(url=res['data']['children'][random.randint(0,99)]['data']['url'])
+                await message.channel.send(embed=embed)
+    elif message.content.startswith('^mushaf') or message.content.startswith('/mushaf'):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://www.searchtruth.org/quran/images1/{message.content[8:]}.jpg') as r:
+                #res = await r.json()
+                embed = discord.Embed(title="Quran Image", description="Here is a quran image", color=0x00ff00)
+                embed.set_image(url=f'https://www.searchtruth.org/quran/images1/{message.content[8:]}.jpg')
+                await message.channel.send(embed=embed)
+    elif message.content.startswith('^ayah') or message.content.startswith('/ayah'):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'http://api.alquran.cloud/v1/ayah/{message.content[6:]}') as r:
+                res = await r.json()
+                embed = discord.Embed(title=f"{res['data']['surah']['name']}:{res['data']['surah']['number']}", description=f"{res['data']['text']}", color=0x00ff00)
+                #embed.set_image(url=res['data']['url'])
+                await message.channel.send(embed=embed)
+    elif message.content.startswith('^tmems') or message.content.startswith('/tmems'):
+         async with aiohttp.ClientSession() as cs:
+                async with cs.get('https://www.reddit.com/r/TechMemes/.json?limit=100') as r:
+                 res = await r.json()
+                 embed = discord.Embed(title="Tech Memes", description="Here is a tech meme", color=0x00ff00)
+                 embed.set_image(url=res['data']['children'][random.randint(0,99)]['data']['url'])
+                 await message.channel.send(embed=embed)                        
+    elif message.content.startswith('^autoayah') or message.content.startswith('/autoayah'):
+        while True:
+            async with aiohttp.ClientSession() as cs:
+                randayah = random.randint(1,6236)
+                async with cs.get(f'http://api.alquran.cloud/v1/ayah/{randayah}') as r:
+                    res = await r.json()
+                    embed = discord.Embed(title=f"{res['data']['surah']['name']}:{res['data']['surah']['number']}", description=f"{res['data']['text']}\n {randayah}:الايه رقم", color=0x00ff00)
+                    await message.channel.send(embed=embed)
+                    await asyncio.sleep(300)
+        #await message.channel.send('^ayah')
+        #asyncio.sleep(300)
+        #asyncio.create_task(message.channel.send('^ayah'))
+        #asyncio.loop = asyncio.get_event_loop()
+        #asyncio.loop.run_until_complete(message.channel.send('^ayah'))
+
 
 
 tvar = "ix"
