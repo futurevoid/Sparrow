24f4d0fe689967d35f588dac7424594ae52ddc34
futurevoid <tamersaeed710@gmail.com>
2022-03-10 09:28:15 +0300

    desc is best

--- Sparrow.py
+++ Sparrow.py
@@ -175,7 +175,7 @@ async def on_message(message):
     elif message.content.startswith('0xavatar') or message.content.startswith('/avatar'):
         try:
             user = message.author
-            embed = discord.Embed(title=f"{message.author.name}#{message.author.discriminator} \n [Avatar Link]({user.avatar_url})", icon_url=user.avatar_url)
+            embed = discord.Embed(title=f"{message.author.name}#{message.author.discriminator}", description=f"[Avatar Link]({user.avatar_url})", icon_url=user.avatar_url)
             embed.set_thumbnail(url=user.avatar_url)
             embed.set_image(url=user.avatar_url)
             embed.set_footer(text=f"Requested by {message.author.name}#{message.author.discriminator}",icon_url=user.avatar_url)
