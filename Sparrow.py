7a7ddca0e4523735b1cc6db73ffc9b7ac9c6905c
futurevoid <tamersaeed710@gmail.com>
2022-03-26 08:00:32 +0300

    using pass to avoid ui issues

--- Sparrow.py
+++ Sparrow.py
@@ -512,10 +512,13 @@ async def on_message(message):
             pass     
         calc_content_urlencoded = urllib.parse.quote(calccontent)   
         try:
-            site_request = requests.get(f"https://api.mathjs.org/v4/?expr={calc_content_urlencoded}")
-            site_request_content = site_request.text
-            embed = discord.Embed(title="Result", description=f"{site_request_content}", color=0x00ff00)
-            await message.channel.send(embed=embed)
+            if calc_content_urlencoded.find('y') or calc_content_urlencoded.find('x'):
+                pass 
+            else:
+                site_request = requests.get(f"https://api.mathjs.org/v4/?expr={calc_content_urlencoded}")
+                site_request_content = site_request.text
+                embed = discord.Embed(title="Result", description=f"{site_request_content}", color=0x00ff00)
+                await message.channel.send(embed=embed)
         except requests.exceptions.RequestException as e:
             await message.channel.send(f'{message.author.mention} {e}')        
 
