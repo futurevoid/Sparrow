f04934463013e063ff39817bfadc893d29d032d8
futurevoid <tamersaeed710@gmail.com>
2022-07-04 21:19:38 +0300

    debug

--- Sparrow.py
+++ Sparrow.py
@@ -351,7 +351,8 @@ async def on_message(message):
                 #discord.ui.button(label="↠" ,style=discord.ui.ButtonStyle.primary)
                 #async def button_callback(self, button, interaction):
                 #    await interaction.response.send_message("You clicked the button!") 
-        await message.channel.send(f'{message.author.mention} {res[0]}')       
+        print(res)
+        await message.channel.send(f'{message.author.mention} {res}')       
     elif message.content.startswith('0xautosabah') or message.content.startswith('/autosabah'):
         await message.channel.purge(limit=1)
         autos='true'
